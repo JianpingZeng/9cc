f80e858e1207d2f16e6b6ba16961b706239b5df3
Mario Huang <guiyang.huang@gmail.com>
2015-03-06 22:19:10 +0800

    seperate mac/linux for cpp again

--- src/cpp/linux.c
+++ src/cpp/linux.c
@@ -2,4 +2,42 @@
  * Configurations for Linux
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
 const char *cpp[] = {"/usr/bin/gcc", "-E", 0};
+
+int callsys(const char *path, char **argv)
+{
+    pid_t pid;
+    int ret = EXIT_SUCCESS;
+    pid = fork();
+    if (pid == 0) {
+	// explicit type cast to make compiler happy
+	execv(path, (char * const *)argv);
+	perror("execv");
+	exit(EXIT_FAILURE);
+    }
+    else if (pid > 0) {
+	// wait for
+	int status;
+	int retpid = waitpid(pid, &status, 0);
+        if (retpid != pid || !WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+	    ret = EXIT_FAILURE;
+	}
+    }
+    else {
+	perror("Can't fork");
+	ret = EXIT_FAILURE;
+    }
+
+    return ret;
+}
+
+int file_exists(const char *path)
+{
+    struct stat st;
+    return stat(path, &st) == 0;
+}
