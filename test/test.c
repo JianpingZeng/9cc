725285a3a36cbb60f808fa4f2b15ecef479a11da
Mario Huang <guiyang.huang@gmail.com>
2015-03-08 12:51:32 +0800

    update test

--- test/test.c
+++ test/test.c
@@ -1,15 +1,15 @@
 
-typedef int myint;
+// typedef int myint;
 // int f(myint, a);
-typedef int T();
+// typedef int T();
 
 // T a[10];
 
-typedef int B[10];
+// typedef int B[10];
 
-B f()
-{
-}
+// B f()
+// {
+// }
 
 int main()
 {
@@ -17,7 +17,30 @@ int main()
     return 0;
 }
 
-typedef int MyInt;
+// typedef int MyInt;
 
-int g(int, int);
-int g(int, MyInt);
+// int g(int, int);
+// int g(int, MyInt);
+
+// int f();
+// int f(float *);
+// int f(double);
+int f(a)
+     short a;
+{
+
+}
+
+// int f(int, ...);
+// int f(float);
+// int f(double);
+int f(short);
+
+// int f(a)
+//      double a;
+// {
+
+ // }
+
+// int g(float);
+// int g(double);
