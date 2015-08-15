e25ffa2537f163256aec84f041cdd73d9823e50d
mario <mohu3g@163.com>
2015-08-15 16:20:06 +0800

    rm lex.h

--- src/cc.h
+++ src/cc.h
@@ -15,7 +15,44 @@
 
 #include "utils.h"
 
-#include "lex.h"
+// lex.c
+#define EOI  -1
+
+enum {
+#define _a(a, b, c, d)  a,
+#define _x(a, b, c, d)  a=d,
+#define _t(a, b, c)     a,
+#include "token.def"
+    TOKEND
+};
+
+struct source {
+    const char *file;
+    unsigned line;
+    unsigned column;
+};
+
+struct token {
+    int id;
+    const char *name;
+    int kind;
+};
+
+extern struct source source;
+extern struct token  *token;
+
+extern bool is_digit(char c);
+extern bool is_hex(char c);
+extern bool is_digithex(char c);
+
+extern void lexer_init();
+extern int  gettok();
+extern struct token *  lookahead();
+extern void expect(int t);
+extern void match(int t, int follow[]);
+extern const char *tname(int t);
+
+// value
 
 union value {
     long long i;
