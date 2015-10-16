5f13cd9f93e85699f3e14e378cc06727c3627685
mario <huangguiyang@linggan.com>
2015-10-16 13:48:49 +0800

    add stdbool.h

---
+++ include/stdbool.h
@@ -0,0 +1,13 @@
+#ifndef _STDBOOL_H
+#define _STDBOOL_H
+
+/** ISO/IEC 9899:1999
+ *  Section 7.16 <stdbool.h>
+ */
+
+#define bool  _Bool
+#define true  1
+#define false 0
+#define __bool_true_false_are_defined  1
+
+#endif
