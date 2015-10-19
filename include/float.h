c5bff0a695f03f16f199da2c54dcd046ab236cde
mario <huangguiyang@linggan.com>
2015-10-19 10:27:04 +0800

    update float.h

---
+++ include/float.h
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) Guiyang Huang, 2015
+ *
+ * ISO/IEC 9899:1999
+ * <float.h>
+ */
+
+#ifndef __FLOAT_H
+#define __FLOAT_H
+
+#define FLT_RADIX 2
+#define FLT_ROUNDS 1
+#define FLT_EVAL_METHOD 0
+
+#define FLT_EPSILON 1.19209290e-7F
+#define DBL_EPSILON 2.2204460492503131e-16
+#define LDBL_EPSILON 1.08420217248550443401e-19L
+
+#define FLT_DIG 6
+#define DBL_DIG 15
+#define LDBL_DIG 18
+
+#define FLT_MANT_DIG 24
+#define DBL_MANT_DIG 53
+#define LDBL_MANT_DIG 64
+#define DECIMAL_DIG 21
+
+#define FLT_MIN 1.17549435e-38F
+#define DBL_MIN 2.2250738585072014e-308
+#define LDBL_MIN 3.36210314311209350626e-4932L
+
+#define FLT_MIN_EXP -125
+#define DBL_MIN_EXP -1021
+#define LDBL_MIN_EXP -16381
+
+#define FLT_MIN_10_EXP -37
+#define DBL_MIN_10_EXP -307
+#define LDBL_MIN_10_EXP -4931
+
+#define FLT_MAX 3.40282347e+38F
+#define DBL_MAX 1.7976931348623157e+308
+#define LDBL_MAX 1.18973149535723176502e+4932L
+
+#define FLT_MAX_EXP 128
+#define DBL_MAX_EXP 1024
+#define LDBL_MAX_EXP 16384
+
+#define FLT_MAX_10_EXP 38
+#define DBL_MAX_10_EXP 308
+#define LDBL_MAX_10_EXP 4932
+
+#endif
