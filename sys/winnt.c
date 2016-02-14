60e11d9d83040b28b291dfb0408a83e108c5c256
mario <huangguiyang@linggan.com>
2016-02-14 18:05:57 +0800

    update placehodler

--- sys/winnt.c
+++ sys/winnt.c
@@ -17,63 +17,63 @@ void setup_sys()
 
 const char * mktmpdir(void)
 {
-    
+    return NULL;
 }
 
 int file_exists(const char *path)
 {
-    
+    return 0;
 }
 
 int file_size(const char *path)
 {
-    
+    return 0;
 }
 
 int isdir(const char *path)
 {
-    
+    return 0;
 }
 
 int rmdir(const char *dir)
 {
-    
+    return 0;
 }
 
 const char *abspath(const char *path)
 {
-    
+    return NULL;
 }
 
 const char *replace_suffix(const char *path, const char *suffix)
 {
-    
+    return NULL;
 }
 
 const char *join(const char *dir, const char *name)
 {
-    
+    return NULL;
 }
 
 char *dirname(const char *path)
 {
-    
+    return NULL;
 }
 
 char *basename(const char *path)
 {
-    
+    return NULL;
 }
 
 // process
 int callsys(const char *file, char **argv)
 {
-    
+    return 0;
 }
 
 int runproc(int (*proc) (void *), void *context)
 {
-    
+    return 0;
 }
 
 // time
