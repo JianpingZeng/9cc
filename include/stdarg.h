aa904e63c72e05a976e10c34cca45b781f3fe48b
mario <huangguiyang@linggan.com>
2016-02-17 14:20:14 +0800

    update stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -8,12 +8,19 @@
 #ifndef __STDARG_H
 #define __STDARG_H
 
-// TODO
-typedef void *__gnuc_va_list;
-typedef __gnuc_va_list va_list;
+typedef __builtin_va_list va_list;
 
-#define va_start(v, l)
-#define va_end(v)   ((void)0)
-#define va_arg(v, l)
+#define va_start(ap, param)   __builtin_va_start(ap, param)
+#define va_end(ap)            __builtin_va_end(ap)
+#define va_arg(ap, type)      __builtin_va_arg(ap, type)
+#define va_copy(dest, src)    __builtin_va_copy(dest, src)
+
+// GNU
+#define __va_copy(d, s)       __builtin_va_copy(d, s)
+
+#ifndef __GNUC_VA_LIST
+#define __GNUC_VA_LIST 1
+#endif
+typedef __builtin_va_list __gnuc_va_list;
 
 #endif
