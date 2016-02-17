25f9594903e39c9affd6e50ba9a032956ac186b9
mario <huangguiyang@linggan.com>
2016-02-17 17:58:07 +0800

    add builtin_init

--- cc.c
+++ cc.c
@@ -15,6 +15,47 @@ static void cc_init(const char *ifile, const char *ofile)
     }
 }
 
+static void define_builtin_type(const char *name, node_t *type)
+{
+    node_t *sym = install(name, &identifiers, GLOBAL);
+    SYM_SCLASS(sym) = TYPEDEF;
+    SYM_TYPE(sym) = type;
+}
+
+static void define_builtin_func(const char *name, node_t *rtype, struct vector *ptypes)
+{
+    node_t *ftype = func_type();
+    _TYPE_TYPE(ftype) = rtype;
+    struct vector *params = vec_new();
+    cc_assert(SCOPE == GLOBAL);
+    enter_scope();
+    for (int i = 0; i < vec_len(ptypes); i++) {
+        node_t *ptype = vec_at(ptypes, i);
+        node_t *param = anonymous(&identifiers, SCOPE);
+        SYM_TYPE(param) = ptype;
+        vec_push(params, param);
+    }
+    exit_scope();
+    _TYPE_PARAMS(ftype) = (node_t **)vtoa(params);
+    node_t *sym = install(name, &identifiers, GLOBAL);
+    SYM_TYPE(sym) = ftype;
+    SYM_DEFINED(sym) = true;
+}
+
+static void builtin_init(void)
+{
+    struct vector *voidptr = vec_new1(ptr_type(chartype));
+    struct vector *voidptr2 = vec_new();
+    vec_push(voidptr2, ptr_type(voidtype));
+    vec_push(voidptr2, ptr_type(voidtype));
+    // __builtin_va_list
+    define_builtin_type("__builtin_va_list", ptr_type(chartype));
+    define_builtin_func("__builtin_va_start", voidtype, voidptr2);
+    define_builtin_func("__builtin_va_end", voidtype, voidptr);
+    define_builtin_func("__builtin_va_copy", voidtype, voidptr2);
+    define_builtin_func("__builtin_va_arg", voidtype, voidptr2);
+}
+
 static void translate(void)
 {
     node_t *tree;
@@ -55,6 +96,7 @@ int cc_main(const char *ifile, const char *ofile)
     cc_init(ifile, ofile);
     input_init(ifile);
     cpp_init(opts.cpp_options);
+    builtin_init();
     
     if (opts.E)
         preprocess();
