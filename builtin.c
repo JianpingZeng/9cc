39db87afa33c8f2cb106dc703a1705f512fe08bd
mario <huangguiyang@linggan.com>
2016-02-18 09:58:02 +0800

    update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,18 +1,21 @@
 #include "cc.h"
 
-static void define_builtin_func(const char *name, node_t *rtype, struct vector *ptypes)
+static void define_builtin_func(const char *name, node_t *rtype, bool varg, ...)
 {
     node_t *ftype = func_type();
     _TYPE_TYPE(ftype) = rtype;
+    if (varg)
+        _TYPE_VARG(ftype) = true;
     struct vector *params = vec_new();
-    cc_assert(SCOPE == GLOBAL);
     enter_scope();
-    for (int i = 0; i < vec_len(ptypes); i++) {
-        node_t *ptype = vec_at(ptypes, i);
+    va_list ap;
+    va_start(ap, varg);
+    for (node_t *ptype; (ptype = va_arg(ap, node_t *)) != NULL; ) {
         node_t *param = anonymous(&identifiers, SCOPE);
         SYM_TYPE(param) = ptype;
         vec_push(params, param);
     }
+    va_end(ap);
     exit_scope();
     _TYPE_PARAMS(ftype) = (node_t **)vtoa(params);
     node_t *sym = install(name, &identifiers, GLOBAL);
@@ -20,19 +23,42 @@ static void define_builtin_func(const char *name, node_t *rtype, struct vector *
     SYM_DEFINED(sym) = true;
 }
 
+/**
+   typedef struct __va_list_tag {
+       unsigned int gp_offset;
+       unsigned int fp_offset;
+       void *overflow_arg_area;
+       void *reg_save_area;
+   } *__builtin_va_list;
+ */
+
+static node_t * define_builtin_va_list(void)
+{
+    struct source src = {.file = "<builtin>", .line = 1, .column = 0};
+    node_t *record = tag_type(STRUCT, BUILTIN_VA_LIST"_tag", src);
+    SYM_DEFINED(record) = true;
+    node_t *type = SYM_TYPE(record);
+    TYPE_SIZE(type) = 24;
+    
+    node_t *array = ptr_type(type);
+    
+    // typedef
+    node_t *sym = install(BUILTIN_VA_LIST, &identifiers, GLOBAL);
+    SYM_SCLASS(sym) = TYPEDEF;
+    SYM_TYPE(sym) = array;
+    
+    return SYM_TYPE(sym);
+}
+
 void builtin_init(void)
 {
-    struct vector *voidptr = vec_new1(ptr_type(chartype));
-    struct vector *voidptr2 = vec_new();
-    vec_push(voidptr2, ptr_type(voidtype));
-    vec_push(voidptr2, ptr_type(voidtype));
+    cc_assert(SCOPE == GLOBAL);
+
+    node_t *va_list_type = define_builtin_va_list();
+    node_t *voidptr = ptr_type(voidtype);
     // __builtin_va_list
-    node_t *sym = lookup(BUILTIN_VA_LIST, identifiers);
-    cc_assert(sym);
-    node_t *type = SYM_TYPE(sym);
-    println("%s", type2s(type));
-    define_builtin_func(BUILTIN_VA_START, voidtype, voidptr2);
-    define_builtin_func(BUILTIN_VA_END, voidtype, voidptr);
-    define_builtin_func(BUILTIN_VA_COPY, voidtype, voidptr2);
-    define_builtin_func(BUILTIN_VA_ARG, voidtype, voidptr2);
+    define_builtin_func(BUILTIN_VA_START, voidtype, true, va_list_type, NULL);
+    define_builtin_func(BUILTIN_VA_END, voidtype, false, va_list_type, NULL);
+    define_builtin_func(BUILTIN_VA_COPY, voidtype, false, voidptr, voidptr, NULL);
+    define_builtin_func(BUILTIN_VA_ARG, voidtype, false, voidptr, voidptr, NULL);
 }
