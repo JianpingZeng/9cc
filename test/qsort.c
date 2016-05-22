cc358903100b3ea39bcadab267c2aba9c0090ac3
huangguiyang <mohu3g@163.com>
2016-05-22 20:40:15 +0800

    add test

---
+++ test/qsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+static int a[] = { 10, -9, 8, 2, 6, 1, 7, 0 };
+
+static void quicksort(int m, int n)
+{
+    int i, j;
+    int v, x;
+    if (n <= m) return;
+    i = m - 1; j = n; v = a[n];
+    while (1) {
+        do i = i+1; while (a[i] < v);
+        do j = j-1; while (a[j] > v);
+        if (i >= j) break;
+        x = a[i]; a[i] = a[j]; a[j] = x;
+    }
+    x = a[i]; a[i] = a[n]; a[n] = x;
+    quicksort(m, j); quicksort(i+1, n);
+}
+
+int main()
+{
+    int len = sizeof(a) / sizeof(a[0]);
+    quicksort(0, len-1);
+    for (int i = 0; i < len; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
