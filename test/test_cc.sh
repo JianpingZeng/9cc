66dd58f800e8371dd385d20aede7afd7f7b55abb
huangguiyang <mohu3g@163.com>
2016-06-01 22:44:35 +0800

    add test_cc.sh

---
+++ test/test_cc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ok=0
+fail=0
+
+rm -rf /tmp/7cc
+mkdir /tmp/7cc
+
+for src in *.c utils/*.c sys/unix.c sys/linux.c
+do
+    f1=/tmp/7cc/`basename $src`.1.s
+    f2=/tmp/7cc/`basename $src`.2.s
+    ./cc1_stage1 -fversion=1 -DCONFIG_LINUX -DCONFIG_COLOR_TERM -DBUILD_DIR='"/mnt/hgfs/下载/7cc"' $src -o $f1
+    ./cc1_stage2 -fversion=1 -DCONFIG_LINUX -DCONFIG_COLOR_TERM -DBUILD_DIR='"/mnt/hgfs/下载/7cc"' $src -o $f2
+    diff $f1 $f2
+    if [ $? -eq 0 ]; then
+        echo -e "\033[0;32m[PASS]\033[0m $src"
+        let "ok += 1"
+    else
+        echo -e "\033[0;31m[FAIL]\033[0m $src"
+        let "fail += 1"
+    fi
+done
+
+rm -rf /tmp/7cc
+
+echo "$ok success, $fail failed."
--- test/test_cpp.sh
+++ test/test_cpp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for src in *.c utils/*.c sys/*.c
+for src in *.c utils/*.c sys/unix.c sys/linux.c
 do
     f1=/tmp/`basename $src`.1.i
     f2=/tmp/`basename $src`.2.i
@@ -8,9 +8,9 @@ do
     ./cc1_stage2 -E -DBUILD_DIR='"/mnt/hgfs/下载/7cc"' $src > $f2
     diff $f1 $f2
     if [ $? -eq 0 ]; then
-       echo "[PASS] $src"
+       echo -e "\033[0;32m[PASS]\033[0m $src"
     else
-       echo "[FAIL] $src"
+       echo -e "\033[0;31m[FAIL]\033[0m $src"
     fi
 done
 
