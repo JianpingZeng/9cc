ff93969dc978fab4d067464f4257a3368b5d4495
mario <huangguiyang@linggan.com>
2016-06-03 09:58:34 +0800

    update test_profile.sh

--- test/test_profile.sh
+++ test/test_profile.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 outdir=profile
+tmpdir=/tmp/7cc
 
 rm -rf $outdir
+rm -rf $tmpdir
 mkdir $outdir
+mkdir $tmpdir
 
 for src in *.c utils/*.c sys/unix.c sys/linux.c
 do
-    f1=/tmp/7cc/`basename $src`.s
+    f1=$tmpdir/`basename $src`.s
     f2=$outdir/`basename $src`.profile.txt
     dir=`pwd`
-    gmon=gmon.out
     echo "Profiling $src ..."
     ./cc1 -fversion=1 -DCONFIG_LINUX -DCONFIG_COLOR_TERM -DBUILD_DIR='"$dir"' $src -o $f1
-    gprof ./cc1 $gmon -p > $f2
+    gprof ./cc1 gmon.out -p > $f2
 done
