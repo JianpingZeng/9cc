da37dd8cc9c2e9e4bdebab926e4d3b8cd6bc629d
mario <mohu3g@163.com>
2016-10-10 18:02:15 +0800

    #tmp: update init.c

--- init.c
+++ init.c
@@ -4,6 +4,89 @@
 /// Initializer
 ///
 
+static bool first_init(struct token *t)
+{
+    return first_expr(t) || t->id == '.' || t->id == '{' || t->id == '[';
+}
+
+static void eat_initializer(void)
+{
+    if (token->id == '[' || token->id == '.') {
+        do {
+            if (token->id == '.') {
+                expect('.');
+                expect(ID);
+            } else {
+                expect('[');
+                intexpr();
+                expect(']');
+            }
+        } while (token->id == '[' || token->id == '.');
+        expect('=');
+    }
+
+    initializer(NULL);
+}
+
+static void eat_initlist(void)
+{
+    do {
+        eat_initializer();
+        if (token->id != ',')
+            break;
+
+        expect(',');
+    } while (first_init(token));
+}
+
+static void struct_init(struct type *ty, long offset, struct init **init, bool brace)
+{
+    
+}
+
+static void array_init(struct type *ty, long offset, struct init **init, bool brace)
+{
+    
+}
+
+static void scalar_init(struct type *ty, long offset, struct init **init)
+{
+    
+}
+
+static void initializer_list1(struct type *ty, long offset, struct init **init)
+{
+    int follow[] = { ',', IF, '[', ID, '.', DEREF, 0 };
+
+    expect('{');
+
+    if (ty) {
+        if (first_init(token)) {
+            if (isstruct(ty) || isunion(ty))
+                struct_init(ty, offset, init, true);
+            else if (isarray(ty))
+                array_init(ty, offset, init, true);
+            else
+                scalar_init(ty, offset, init);
+
+            if (token->id == ',')
+                expect(',');
+
+            if (first_init(token)) {
+                warning("excess element in %s initializer at '%s'",
+                        TYPE_NAME(ty), tok2s(token));
+                eat_initlist();
+            }
+        } else {
+            error("expect initializer at '%s'", tok2s(token));
+        }
+    } else {
+        eat_initlist();
+    }
+
+    match('}', follow);
+}
+
 /// initializer:
 ///   assignment-expression
 ///   '{' initializer-list '}'
@@ -38,10 +121,11 @@ struct expr *initializer(struct type * ty)
 ///
 struct expr *initializer_list(struct type * ty)
 {
-    int follow[] = { ',', IF, '[', ID, '.', DEREF, 0 };
     struct expr *ret = ast_expr(COMPOUND, ty, NULL, NULL);
+    struct init *init = NULL;
 
-    // TODO: 
+    initializer_list1(ty, 0, &init);
+    ret->u.inits = init;
     
     return ret;
 }
