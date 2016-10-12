2c77f5fc4fdd355c85dfae0822faab14a7c63d41
mario <mohu3g@163.com>
2016-10-12 18:10:40 +0800

    cpp_warningf/cpp_errorf/cpp_fatalf => cpp_warning_at/cpp_error_at/cpp_fatal_at

--- libcpp/error.c
+++ libcpp/error.c
@@ -9,9 +9,7 @@
 
 #define MAX_ERRORS 1
 
-static void cc_print_lead(int tag,
-                          const char *file, unsigned int line, unsigned int column,
-                          const char *fmt, va_list ap)
+static void cc_print_lead(int tag, struct source src, const char *fmt, va_list ap)
 {
     const char *lead;
     switch (tag) {
@@ -28,43 +26,39 @@ static void cc_print_lead(int tag,
         assert(0);
     }
 
-    fprintf(stderr, CLEAR "%s:%u:%u:" RESET " %s ", file, line,
-            column, lead);
+    fprintf(stderr, CLEAR "%s:%u:%u:" RESET " %s ",
+            src.file, src.line, src.column, lead);
     fprintf(stderr, CLEAR);
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, RESET);
     fprintf(stderr, "\n");
 }
 
-void cpp_warningf(const char *file, unsigned int line, unsigned int column,
-                  const char *fmt, ...)
+void cpp_warning_at(struct source src, const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    cc_print_lead(WRN, file, line, column, fmt, ap);
+    cc_print_lead(WRN, src, fmt, ap);
     va_end(ap);
 }
 
-void cpp_errorf(const char *file, unsigned int line, unsigned int column,
-                const char *fmt, ...)
+void cpp_error_at(struct source src, const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    cc_print_lead(ERR, file, line, column, fmt, ap);
+    cc_print_lead(ERR, src, fmt, ap);
     va_end(ap);
-    ++cpp_file->errors;
-    if (cpp_file->errors >= MAX_ERRORS) {
+    if (cpp_file->errors++ >= MAX_ERRORS) {
         fprintf(stderr, "Too many errors.\n");
         exit(EXIT_FAILURE);
     }
 }
 
-void cpp_fatalf(const char *file, unsigned int line, unsigned int column,
-                const char *fmt, ...)
+void cpp_fatal_at(struct source src, const char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
-    cc_print_lead(FTL, file, line, column, fmt, ap);
+    cc_print_lead(FTL, src, fmt, ap);
     va_end(ap);
     exit(EXIT_FAILURE);
 }
--- libcpp/internal.h
+++ libcpp/internal.h
@@ -61,20 +61,16 @@ extern void imap_dump(struct imap *imap);
 // error.c
 enum { WRN = 1, ERR, FTL };
 
-extern void cpp_warningf(const char *file, unsigned int line, unsigned int column,
-                         const char *fmt, ...);
-extern void cpp_errorf(const char *file, unsigned int line, unsigned int column,
-                       const char *fmt, ...);
-extern void cpp_fatalf(const char *file, unsigned int line, unsigned int column,
-                       const char *fmt, ...);
+struct source;
+
+extern void cpp_warning_at(struct source src, const char *fmt, ...);
+extern void cpp_error_at(struct source src, const char *fmt, ...);
+extern void cpp_fatal_at(struct source src, const char *fmt, ...);
 
 #define SAVE_ERRORS    unsigned int __err = cpp_file->errors
 #define NO_ERROR       (__err == cpp_file->errors)
 #define HAS_ERROR      (__err != cpp_file->errors)
 
-#define cpp_warning_at(s, ...)   cpp_warningf(s.file, s.line, s.column, __VA_ARGS__)
-#define cpp_error_at(s, ...)     cpp_errorf(s.file, s.line, s.column, __VA_ARGS__)
-#define cpp_fatal_at(s, ...)     cpp_fatalf(s.file, s.line, s.column, __VA_ARGS__)
 #define cpp_warning(...)         cpp_warning_at(source, __VA_ARGS__)
 #define cpp_error(...)           cpp_error_at(source, __VA_ARGS__)
 #define cpp_fatal(...)           cpp_fatal_at(source, __VA_ARGS__)
