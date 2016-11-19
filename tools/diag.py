13858af38cfe4e06681a5679707c7d5c581f3546
mario <mohu3g@163.com>
2016-11-19 09:34:38 +0800

    update diagmsg.txt; add tools/diag.py placeholder

---
+++ tools/diag.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+# generates diag.h and diag.c from diagmsg.txt.
+
+import os
+
+def gen_diag_h(arg):
+    pass
+
+
+def gen_diag_c(arg):
+    pass
+
+
+def main(arg):
+    pass
+
+
+if __name__ == '__main__':
+    main()
